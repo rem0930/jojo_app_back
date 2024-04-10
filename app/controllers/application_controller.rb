bb70d2f11741c2f1aeb5da13ac08fbaa130e6d3f
Rem Araki <rem.araki0930@gmail.com>
2024-04-11 01:39:44 +0900

    Update application_controller.rb
    modify decode_token and generate_token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
 class ApplicationController < ActionController::API
+    before_action :authenticate_request
 
     # リクエストヘッダーからトークンを抽出するためのやつ
     def extract_token_from_header
         # リクエストヘッダーから 'Authorization' フィールドの値を取得
         header = request.headers['Authorization']
         # ヘッダーが存在する場合、スペースで区切られた最後の要素（つまりトークン）を取得
-        token = header.split(' ').last if header.present?
+        token = header&.split(' ')&.last
         puts "Extracted token: #{token}"
         token
     end
@@ -14,9 +15,9 @@ class ApplicationController < ActionController::API
     # JWTトークンをデコードしてペイロードを取得
     def decode_token(token)
         # JWT.decode メソッドを使用して、与えられたトークンをデコード
-        # 第1引数にはデコードするトークン、第2引数にはシークレットキー（ここでは nil)
-        # 第3引数にはトークンの検証を行うかどうかを示すブール値（ここではfalse）。
-        decoded_token = JWT.decode(token, nil, false)
+        # 第1引数にはデコードするトークン、第2引数にはシークレットキー
+        # 第3引数にはトークンの検証を行うかどうかを示すブール値（true）。
+        decoded_token = JWT.decode(token, Rails.application.config.jwt_secret_key, true)
         puts "Decoded token: #{decoded_token.inspect}"
         # デコードされたトークン（ペイロード）が返されます。
         decoded_token
@@ -27,7 +28,7 @@ class ApplicationController < ActionController::API
 
     def generate_jwt_token(user)
         # 与えられたユーザーのIDを含むハッシュ
-        payload = { user_id: user.id }
+        payload = { user: user.as_json(only: [:id, :email, :username]) }
         # ペイロードをトークンにエンコード.第1引数にはペイロード、第2引数にはシークレットキーを指定
         # このシークレットーキーはトークンの署名に使用され、トークンの検証時に必要
         JWT.encode(payload, Rails.application.config.jwt_secret_key)
@@ -39,14 +40,22 @@ class ApplicationController < ActionController::API
         decoded_token = decode_token(extract_token_from_header)
         return nil if decoded_token.blank?
 
-        user_id = decoded_token.first['user_id']
-        @current_user = User.find_by(id: user_id)
+        user_data = decoded_token.first['user']
+        @current_user = User.find_by(id: user_data['id'])
     end
 
-      # 認証メソッド(posts_controller.rbで使用)
-    def authenticate_user
+    # 認証メソッド
+    def authenticate_request
         unless current_user
             render json: { error: 'ログインする必要があります' }, status: :unauthorized
+        end
+    end
+
+    private
+    
+    def authenticate_user
+        unless current_user
+            current_user.present?
         end
     end
 end
